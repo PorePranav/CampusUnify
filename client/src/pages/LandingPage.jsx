c458b207e8755c96016df370bded067cb460e79e
Pranav Pore <pranav@pranavpore.com>
2024-10-08 22:23:10 +0530

    chore: performed linting + formatting

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -8,7 +8,7 @@ import { useContext } from 'react';
 
 export default function LandingPage() {
   const { isLoading, latestEvents } = useLatestEvents();
-  const { isDarkMode, setIsDarkMode } = useContext(Context);
+  const { isDarkMode } = useContext(Context);
 
   return (
     <PageLayout>
