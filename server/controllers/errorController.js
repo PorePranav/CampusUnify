50885ed668bf043be2d90ab646d49d210c441a12
Pranav Pore <pranav@pranavpore.com>
2024-10-10 14:06:58 +0530

    fixed error handler

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -62,7 +62,7 @@ const handleJWTError = () =>
 const handleExpiredTokenError = () =>
   new AppError(`Token expired. Please log in again`, 401);
 
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
